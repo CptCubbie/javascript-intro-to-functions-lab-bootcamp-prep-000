af57b1fd2fef68a627bf77b4bfa421fc0db7e262
Brittany Tinnin <bntinnin7@gmail.com>
2018-03-02 19:53:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function shout(string) {
   return string.toUpperCase();
 }
-
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
